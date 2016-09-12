2f6baf0ef0bd73b4c7b95c0659f2c9ca05ce5e77
Johannes Oos <j.oos@hotmail.de>
2016-09-12 19:00:55 +0200

    create config

---
+++ trading_ig_config.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 12 18:56:55 2016
+
+@author: Oos
+"""
+
+#log in information
+# this also determines whether real money is used demo or live
+# so better be sure about $$$$   :-)
+
+class config(object):
+    username = "" #insert username
+    password = "" #insert password
+    api_key = "" #insert api key
+    api_name = "" # insert api name if availabe - not necassary
+    acc_type = "DEMO" # LIVE / DEMO
+    acc_number = "" # insert account number
+    acc_name = "" # insert account name
